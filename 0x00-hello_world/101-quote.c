d3134483f5ef453a732b73941a15a98542d882e4
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-16 19:16:12 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,11 @@
 #include<unistd.h>
 /**
 * main: entry of program
-* Return: -> 1 (success)
+* Return: 1 (success)
 */
 
 int main(void)
 {
-<<<<<<< HEAD
-	  fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-=======
   fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
->>>>>>> aeb59190bcaf96455154f0f7f0d123591e9756f5
   return (1);
 }

f48dcbdc2b5fb7caf4b6f7a6e2f54e60a51fb205
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-16 18:52:36 +0200

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+* main: entry of program
+* Return: 1 (success)
+*/
+
+int main(void)
+{
+  fput("printfand that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+  return (1);
+}

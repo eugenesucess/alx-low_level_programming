86fe6d8c8778de0d3943b7ba07419594ffd2fe18
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-02-16 09:12:43 -0800

    PRINT C SOMETHING

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include<unistd.h>
 /**
 * main: entry of program
-* Return: 1 (success)
+* Return: -> 1 (success)
 */
 
 int main(void)
 {
-  fput("printfand that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	  fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
   return (1);
 }

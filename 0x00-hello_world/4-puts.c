fd85357f8076b60372da455e426529cbc93f8cf1
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-02-16 07:30:50 -0800

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-/**
-*Main function is the entry
-*return (0) when success
-*/
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }

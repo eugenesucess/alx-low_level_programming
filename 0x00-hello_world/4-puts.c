dc790f198bfbabc4839fb28007bb7b8d3672b6a4
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-16 17:26:04 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void){
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+/**
+*Main function is the entry
+*return (0) when success
+*/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return(0);
 }

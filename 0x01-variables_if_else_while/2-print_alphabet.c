b116231ee34747fa6449ea80b44e5c2532d47d40
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-17 18:43:06 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
 /**
-* main - Prints the alphabet
-* return : 0 success
+*main - Prints the alphabet
+*return : 0 success
 */
-
 int main(void)
 {
 	char a;

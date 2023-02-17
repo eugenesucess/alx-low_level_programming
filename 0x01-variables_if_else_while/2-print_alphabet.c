1619656a6d98eff71ed7f12fa99a1730980c8ff7
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-17 18:39:58 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,10 @@
 
 int main(void)
 {
-    char a;
-    for(a = 'a'; a <= 'z'; a++)
-    putchar(a);
-    putchar('\n');
-  
-	  return 0;
+	char a;
+
+	for(a = 'a'; a <= 'z'; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
 }

de7ce88571b3319254a095e1eb63576ca8c5d6e7
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-17 18:52:11 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,12 @@
 
 int main(void) /* main function*/
 {
-  char a;
-  for(a = 'a'; a <= 'z'; a++)
-  putchar(a);
-  for(a = 'A'; a <= 'Z'; a++)
-  putchar(a);
-  putchar('\n');
-  return 0;
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+for (a = 'A'; a <= 'Z'; a++)
+putchar(a);
+putchar('\n');
+return (0);
 }

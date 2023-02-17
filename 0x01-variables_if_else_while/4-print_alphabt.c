07d4a363c055b7d357f9c8a4ae8ebeac49f5cad6
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-17 18:55:25 +0200

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void) /* main function*/
+{
+    char a;
+    for(a = 'a'; a <= 'z'; a++)
+    {
+        if( a == 'e' || a == 'q'){
+            continue;
+            
+        }
+    putchar(a);
+    putchar('\n');
+    }
+	return (0);
+}

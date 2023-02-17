a2fece19f8baa42c18a8266f02ed20b5ee0a5551
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-17 18:35:27 +0200

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+* main - entry 
+* return : 0 success
+*/
+
+int main(void)
+{
+    char a;
+    for(a = 'a'; a <= 'z'; a++)
+    putchar(a);
+    putchar('\n');
+  
+	  return 0;
+}

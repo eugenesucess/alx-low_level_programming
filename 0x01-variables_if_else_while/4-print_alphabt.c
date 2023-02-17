97533a08ad8b12943abe7430e56263fd7a59b8a5
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-17 18:59:24 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,15 +4,15 @@
 
 int main(void) /* main function*/
 {
-    char a;
-    for(a = 'a'; a <= 'z'; a++)
-    {
-        if( a == 'e' || a == 'q'){
-            continue;
-            
-        }
-    putchar(a);
-    putchar('\n');
-    }
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	if ( a == 'e' || a == 'q'){
+	    continue; 
+	}
+	putchar(a);
+	}
+	putchar('\n');
+
 	return (0);
 }

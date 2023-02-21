23a0ba379b9865ac8b3f04e71a512aa30941800e
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-02-20 23:59:52 -0800

    check if it is lowecase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower : check lower case
+ *  return 0 (not lower) 1 (lower)
+ *
+ */
+
+int _islower(int c)
+{
+	if((c >=97) && (c<=122))
+	{
+		return (1);
+	}
+	return (0);
+}

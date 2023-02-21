58f56cfe1fbdabec115729edc8e79e8a6ea5bfad
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-21 15:26:51 +0200

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry of program
+ * return 0 (success)
+ */
+ 
+int main() {
+    int initial = 0;
+    int n1 = 0, n2 = 1;
+    int nx = n1 + n2;
+    
+    while(initial <= 50)
+    {
+        if (initial == 50)
+        {
+            printf("%d \n", nx);
+        }
+        else
+        {
+            printf("%d, \n", nx);
+        }
+        n1 = n2;
+        n2 = nx;
+        
+        nx = n1 + n2;
+        initial++;
+    }
+    return 0;
+}

d0f93909f359e72a0ff7ad3502329c6e8066019e
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-21 15:43:13 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
  * Return: void
  */
 
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 	char start, end = 'z';
 	int i;
-	
-	for (i = 0; i<10; i++)
+
+	for (i = 0; i < 10; i++)
 	{
-		for (start = 'a'; start<= end; start++)
+		for (start = 'a'; start <= end; start++)
 		{
 			_putchar(start);
 		}

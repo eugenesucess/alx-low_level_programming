70d203a79ebc6064ec34426314b9fca1d0bd3c8d
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-21 16:04:01 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
+ * short description
  * print_sign : print according to size
- * return 1 or 0 or -1
- *
+ * Return: integer
+ * parameter n : will be parsed
  */
 
 int print_sign(int n)
 {
-	if( n>0 )
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if ( n == 0) 
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else {
+	else
+	{
 		_putchar('-');
 		return (-1);
 	}

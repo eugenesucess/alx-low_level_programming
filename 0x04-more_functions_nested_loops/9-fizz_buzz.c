660543883ee3caa8e0daffb50640b6e6775f068b
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-02-23 08:15:08 -0800

    print fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int start;
+
+	for (start = 1; start <= 100; start++)
+	{
+		if (start % 3 == 0 && start % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		if (start % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		if (start % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		printf(" ");
+	}
+	return (0);
+}

4a8dcfdb76f92bc40aeaad71b4f1fcd7d8b7a71e
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-02-23 09:11:10 -0800

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,32 +9,32 @@
 
 void print_diagonal(int n)
 {
-  int count = 0, start = 0, end;
-  
-  if (n <= 0)
-  {
-    _putchar('\n');
-  }
-  else
-  {
-    while (count < n)
-    {
-      end = count;
-      while (start <= end)
-      {
-        if (start == end)
-        {
-          _putchar('\\');
-          _putchar('\n');
-        }
-        else
-        {
-          _putchar(' ');
-        }
-        start++;
-      }
-      start = 0;
-      count++;
-    }
-  }
+	int count = 0, start = 0, end;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (count < n)
+		{
+			end = count;
+			while (start <= end)
+			{
+				if (start == end)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			start++;
+			}
+		start = 0;
+		count++;
+		}
+	}
 }

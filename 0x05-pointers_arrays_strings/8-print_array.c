7093cddce966ff8bba2435c0ea5f08224ed4791f
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-02-28 00:12:46 +0200

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+/**
+* print_ array: print array
+* return : void
+
+void print_array(int *a, int n)
+{
+    int i;
+    for (i = 0; i <= n; i++)
+    {
+        printf("%d, ", a[i]);
+    }
+}

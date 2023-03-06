e9c007e440cfc6d80a65b24c2a02adfaf14d2e19
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-03-06 15:53:57 +0200

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy -> copies a certain number of char
+ * @dest: target string 
+ * @src: character of characters to be copied
+ * @n: number of characters to be copied
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    memcpy(dest, src, n);
+    return (dest);
+}

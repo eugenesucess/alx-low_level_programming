c46e63b1f428ddebe624181047dc24d73fcd1c52
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-06 13:24:10 -0800

    Copy method memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,13 +4,14 @@
 
 /**
  * _memcpy -> copies a certain number of char
- * @dest: target string 
+ * @dest: target string
  * @src: character of characters to be copied
  * @n: number of characters to be copied
+ * Return: character
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    memcpy(dest, src, n);
-    return (dest);
+	memcpy(dest, src, n);
+	return (dest);
 }

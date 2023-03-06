a941e8619e507913ef9c3bc6efb8d7c12a833648
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-06 14:33:37 -0800

    return substring when exist

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,14 +4,15 @@
 
 /**
  * _strstr -> return exact needle if is in haystack
- * @haystack: string to find from 
+ * @haystack: string to find from
  * @needle: character to be located
  * Return: character
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-    char *rs;
-    rs = strstr(haystack, needle);
-    return (rs);
+	char *rs;
+
+	rs = strstr(haystack, needle);
+	return (rs);
 }

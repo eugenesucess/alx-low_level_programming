1a3a27a50b3cbefadf9b2e48b330398819ac163a
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-03-06 16:55:20 +0200

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr -> return exact needle if is in haystack
+ * @haystack: string to find from 
+ * @needle: character to be located
+ * Return: character
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+    char *rs;
+    rs = strstr(haystack, needle);
+    return (rs);
+}

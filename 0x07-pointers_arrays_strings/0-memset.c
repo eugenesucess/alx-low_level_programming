95384c827f8d8d635cf3eb08f6e81905d518100a
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-06 05:31:24 -0800

    memset copy number of chars

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+/**
+ * _memset -> copies a certain number of char
+ * @s: target string
+ * @b: character of characters to be copied
+ * @n: number of characters to be copied
+ * Return: character
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+}

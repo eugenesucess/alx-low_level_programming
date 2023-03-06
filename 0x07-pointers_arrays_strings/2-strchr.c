a54cf2f2b9ba6be44210a1e7185164b9029e3654
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-03-06 16:19:00 +0200

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr -> copies a certain number of char
+ * @s: string to find from 
+ * @c: character to be located
+ * Return: character
+ */
+
+char *_strchr(char *s, char c)
+{
+    strchr(s, c);
+    return (s);
+}

a484c84057c8febe6811afb51bd7cf6380a37a8d
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-06 14:30:54 -0800

    strpbrk hot fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,14 +4,15 @@
 
 /**
  * _strpbrk -> return number of similar char
- * @s: string to find from 
- * @acccept: character to be located
+ * @s: string to find from
+ * @accept: character to be located
  * Return: character
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-    char *rs;
-    rs = strpbrk(s, accept);
-    return (rs);
+	char *rs;
+
+	rs = strpbrk(s, accept);
+	return (rs);
 }

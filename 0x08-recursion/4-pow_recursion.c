e5b96ccecd8d7c7a5b6cd8c962eae2a77afdbff4
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-03-08 10:32:08 +0200

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return power x by y
+ * @x: input
+ * @y: input
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+    {
+        return 1;
+    }
+    return (x * _pow_recursion(x, y - 1));
+}

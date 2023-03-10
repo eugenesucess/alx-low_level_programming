9f64f06a560895681ac3aefe97bb597562ba6537
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-10 05:05:44 -0800

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  * main -> entry
+ *   * @argc: program counts
+ *    * @argv: array of arguments
+ *     * Return: integer
+ *      */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 0 || argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!(atoi(argv[i])))
+			{
+				printf("%s\n","Error");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+
+		printf("%d\n", sum);
+	}
+	return (0);
+}
+

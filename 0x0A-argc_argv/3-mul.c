fcdf311d2a22524271d35a814d27c41c5badd8f1
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-10 04:10:49 -0800

    hot fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,6 +11,12 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
 	for (i = 1; i < argc; i++)
 	{
 		mul *= atoi(argv[i]);

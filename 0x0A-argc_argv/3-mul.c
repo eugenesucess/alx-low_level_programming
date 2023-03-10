6a7bff51d46ab076cc0bf6c895c1f110753b299b
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-10 03:51:30 -0800

    mult

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+	for (i = 1; i < argc; i++)
+	{
+		mul *= atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}

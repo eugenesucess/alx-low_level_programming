1f58e4082150f8d27e6bbb745af0ca862774f6e1
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-10 05:18:05 -0800

    hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 
 /**
- *  * main -> entry
- *   * @argc: program counts
- *    * @argv: array of arguments
- *     * Return: integer
+ * main -> entry
+ * @argc: program counts
+ * @argv: array of arguments
+ * Return: integer
  */
 
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc == 0 || argc == 1)
+	if (argc == 1)
 	{
 		printf("%d\n", 0);
 		return (1);

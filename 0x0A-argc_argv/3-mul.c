8ca08a169e6937a5177cdab3a425f4b3e876d151
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-10 03:54:13 -0800

    hot fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * main -> entry
+ * @argc: program counts
+ * @argv: array of arguments
+ * Return: integer
+ */
 int main(int argc, char *argv[])
 {
 	int i, mul = 1;
+
 	for (i = 1; i < argc; i++)
 	{
 		mul *= atoi(argv[i]);

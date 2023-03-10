a585c19aa8355ca2221ce0af88aaa4d073018877
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-10 03:16:52 -0800

    whats name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -< entry point
+ * @argc: value of count
+ * @argv: array of lines from command
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}

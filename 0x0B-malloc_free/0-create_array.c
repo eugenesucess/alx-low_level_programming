1b040869abc523eaaa4e39ae632bfea85624590c
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-13 05:19:38 -0700

    First task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * create_array _. create array
+ * @size: size pf array
+ * @c: characterr
+ * Return: pointer to reserved space
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	int i;
+
+	array = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}

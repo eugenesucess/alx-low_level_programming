e55d78e647bf7786a193cffac71f63454cff2482
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-03-13 05:33:24 -0700

    concat string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * str_concat -> return cononcanated strngs
+ * @s1: param1
+ * @s2: param2
+ * Return: char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int sz1 = 0, sz2 = 0;
+	char *ptr, *rs;
+
+	ptr = s1;
+	while (*ptr++)
+	{
+		sz1++;
+	}
+
+	while (*ptr++)
+	{
+		sz2++;
+	}
+
+	rs = malloc(sz1 + sz2 + 1);
+	if (!rs)
+		return (NULL);
+
+	ptr = rs;
+	while (*s1)
+	{
+		*ptr++ = *s1++;
+	}
+	while (*s2)
+	{
+		*ptr++ = *s2++;
+	}
+	*ptr = 0;
+
+	return (rs);
+}
